12a9796bc113be55f7d6a20b01de89f4f18c9efe
ykeanu <ykhernan@ucla.edu>
2017-11-05 17:28:01 -0800

    Added starter functions

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-// ==================== I. NODE PACKAGES ====================
+// ==================== NODE PACKAGES ====================
+const inquirer = require('inquirer');
+const app = require('./game.js');
+let game = {};
 
-// ==================== II. FUNCTIONS && DECLARATIONS ====================
+// ==================== FUNCTIONS && DECLARATIONS ====================
 
-// ==================== III. MAIN PROCESS ====================
+function startGame() {
+    inquirer.prompt([ /* Pass your questions in here */ 
+    	{
+            type: 'list',
+            name: 'letter',
+            message: 'Would you like to play hangman?',
+            choices: ['YES', 'NO']
+        }
+    ]).then(function(answer) {
+        app new
+        app.startGame();
+        userInput();
+
+        // Use user feedback for... whatever!!
+    });
+
+}
+
+function userInput() {
+	console.log("HI");
+}
+
+
+
+// ==================== MAIN PROCESS ====================
+startGame();
--- game.js
+++ game.js
@@ -2,4 +2,16 @@
 
 // ==================== II. FUNCTIONS && DECLARATIONS ====================
 
+function Hangman() {
+	this.word = new word(),
+	this.displayMessage = '';
+	this.guesses = guess,
+	this.word = new word(),
+}
+
+Hangman.prototype.startGame = function() {
+
+}
+
 // ==================== III. MAIN PROCESS ====================
+
